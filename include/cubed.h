4402cf391b1bda34fdc78424048eacbed010ec51
Mailys Riviere <mailys.riviere@gmail.com>
2025-05-27 17:46:51 +0200

    create header, add library

---
+++ include/cubed.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cubed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 15:41:55 by mairivie          #+#    #+#             */
+/*   Updated: 2025/05/27 17:45:30 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <math.h>
+# include <unistd.h> // write close read
+# include <stdio.h> // printf perror
+# include <stdlib.h> // malloc free exit
+# include <fcntl.h> // open
+# include <sys/stat.h> // open
+# include <sys/time.h> // gettimeofday
+# include <string.h> // strerror
+# include "../minilibx-linux/mlx.h"
+# include "../libft/libft.h"
+
+
