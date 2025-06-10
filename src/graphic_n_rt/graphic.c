282795b7621ff7331e3c3d6db5da95acdbec8413
Mailys Riviere <mailys.riviere@gmail.com>
2025-06-10 18:44:51 +0200

    test color by MR ok, add maps color in painting_ft

--- src/graphic_n_rt/graphic.c
+++ src/graphic_n_rt/graphic.c
@@ -6,19 +6,20 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 18:33:51 by mairivie          #+#    #+#             */
-/*   Updated: 2025/06/09 12:58:08 by mairivie         ###   ########.fr       */
+/*   Updated: 2025/06/10 18:20:22 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/cubed.h"
+#include "../../include/cubed.h"
 
 int	what_color_is_this_pixel(double x, double y, t_infra *infra)
 {	
 	int	color;
 
-	color = 7556677;
+	// color = 7556677;
+	color = infra->data->game.floor_color;
     (void)x;
 	if (y <= HEIGHT / infra->ratio)
-		color = 100000;
+		color = infra->data->game.ceiling_color;
 	return (color);
 }
