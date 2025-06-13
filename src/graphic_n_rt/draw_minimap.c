ba10c64aaf46bc4ffe786caf99cb18eac11ee274
Sara Bellili <sabellil@f1r9s8.clusters.42paris.fr>
2025-06-13 10:59:52 +0200

    Carte en pointilles

--- src/graphic_n_rt/draw_minimap.c
+++ src/graphic_n_rt/draw_minimap.c
@@ -6,7 +6,7 @@
 /*   By: sabellil <sabellil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 11:42:21 by sabellil          #+#    #+#             */
-/*   Updated: 2025/06/13 10:37:20 by sabellil         ###   ########.fr       */
+/*   Updated: 2025/06/13 10:59:24 by sabellil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	draw_minimap(t_data *data)
 	t_img	*img;
 
 	img = data->infra.img_nxt;
-	tile_size = 10;
+	tile_size = 15;
 	y = 0;
 	while (y < data->game.map_height)
 	{
@@ -31,12 +31,12 @@ void	draw_minimap(t_data *data)
 		{
 			current_case = data->game.map[y][x];
 			if (current_case == '1')
-				color = 0x0000;
+				color = 0x222222;
 			if (current_case == '0')
-				color = 0xffff;
+				color = 0xffffff;
 			else
-				color = 0x0000;
-			draw_square(img, data->game.pos_x * tile_size, data->game.pos_y * tile_size, tile_size, color);
+				color = 0x222222;
+			draw_square(img, x * tile_size, y * tile_size, tile_size, color);
 			x++;
 		}
 		y++;
@@ -55,30 +55,9 @@ void	draw_square(t_img *img, int x, int y, int size, int color)
 		j = 0;
 		while (j < size)
 		{
-			put_pixel(img, color, x + j, y + i);
+			put_pixel(img, color, x, y);
 			j++;
 		}
 		i++;
 	}
 }
-// DESSINER LA MINIMAP ET LE JOUEUR
-
-//void	draw_minimap(t_data *data)//dessine pixel par pixel un carre colore
-//on commence a dessiner a partir de la premiere ligne de la map 
-//on dessine sur img_nxt - celle qui n'est pas encore affichee
-//boucle pour parcourir la hauteur 
-	//une boucle parcourant chaque case en fonction de la largeur
-			//si 1 alors mur noir
-			// si 0 sol blanc
-			//si espace : noir 
-	//appel draw square
-//appel draw square pour dessiner le joueur en rouge : 
-// x position horizontale du joueur * taille du pixel, la meme pour y en vertical
-//la taille du carre qui represente le joueur
-//la couleur du caree
-
-// void	draw_square()//on dessine chaque pixel du joueur
-// {
-// 	//boucle pour parcourir les pixels a remplir pour le joueur
-// 	// appel put_pixel();//mettre une couleur sur le pixel  x+j (la position horizontal du pixel)
-// }
