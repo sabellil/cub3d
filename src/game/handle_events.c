184cbbd39f7532b53413bed4cd424f05a7918828
Mailys RIVIERE <mailys.riviere@gmail.com>
2025-07-15 16:58:03 +0200

    Norm okay handle events et raycast generator

--- src/game/handle_events.c
+++ src/game/handle_events.c
@@ -2,88 +2,40 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   handle_events.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+        
-	+:+     */
-/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+      
-	+#+        */
-/*                                                +#+#+#+#+#+  
-	+#+           */
-/*   Created: 2025/06/23 12:56:53 by mairivie          #+#    #+#             */
-/*   Updated: 2025/06/23 12:56:53 by mairivie         ###   ########.fr       */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 16:50:03 by mairivie          #+#    #+#             */
+/*   Updated: 2025/07/15 16:50:03 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cubed.h"
 
-
-int	key_up(t_infra *infra)
-{
-	infra->data->game.pos_y -= 0.2;
-	return (SUCCESS);
-}
-
-int	key_down(t_infra *infra)
-{
-	infra->data->game.pos_y += 0.2;
-	return (SUCCESS);
-}
-
-int	key_left(t_infra *infra)
-{
-	infra->data->game.pos_x -= 0.2;
-	return (SUCCESS);
-}
-
-int	key_right(t_infra *infra)
-{
-	infra->data->game.pos_x += 0.2;
-	return (SUCCESS);
-}
-
-int	key_turn_left(t_infra *infra)
-{
-	infra->ratio = infra->ratio + 0.2;
-	return (SUCCESS);
-}
-
-int	key_turn_right(t_infra *infra)
-{
-	if (infra->ratio > 1.3)
-		infra->ratio = infra->ratio - 0.2;
-	return (SUCCESS);
-}
-
 void	ft_setup_hooks(t_data *data)
 {
-	t_infra *infra;
+	t_infra	*infra;
 
 	infra = &data->infra;
-	mlx_hook(infra->win, DestroyNotify, StructureNotifyMask, close_window,
-			infra);
+	mlx_hook(infra->win, DestroyNotify, StructureNotifyMask,
+		close_window, infra);
 	mlx_key_hook(infra->win, handle_keypress, infra);
-	//mlx_mouse_hook(infra->win, handle_mouse_event, infra);
 }
-
-// #define XK_KP_Left                       0xff96
-// #define XK_KP_Up                         0xff97
-// #define XK_KP_Right                      0xff98
-// #define XK_KP_Down                       0xff99
 
 int	handle_keypress(int keycode, t_infra *infra)
 {
 	if (keycode == XK_Escape)
 		close_window(infra);
 	if (keycode == XK_w || keycode == XK_W)
-		move_up(infra); //bas = moove y--
+		move_up(infra);
 	if (keycode == XK_s || keycode == XK_S)
-		move_down(infra); //haut = moove y++
+		move_down(infra);
 	if (keycode == XK_d || keycode == XK_D)
-		move_right(infra); //key right droite = moove x++
+		move_right(infra);
 	if (keycode == XK_a || keycode == XK_A)
-		move_left(infra); //key righht gauche = moove x--
+		move_left(infra);
 	if (keycode == XK_Left || keycode == XK_Right)
 		move_turn(infra, keycode);
-	printf("x: %f / y: %f / A: %f \n", infra->data->game.pos_x, infra->data->game.pos_y, infra->data->game.angle);
 	return (SUCCESS);
 }
 
@@ -93,18 +45,3 @@ int	close_window(t_infra *infra)
 	free_mlx_and_data(infra);
 	exit(SUCCESS);
 }
-
-/*int	handle_mouse_event(int button, int x, int y, t_infra *infra)
-{
-	(void)x;
-	(void)y;
-	if (button == SCROLL_UP)
-	{
-        printf("scroll up\n");
-	}
-	if (button == SCROLL_DOWN)
-	{
-        printf("scroll up\n");
-	}
-	return (SUCCESS);
-}*/
--- src/game/ray_casting_generator.c
+++ src/game/ray_casting_generator.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ray_tracing_generator.c                            :+:      :+:    :+:   */
+/*   ray_casting_generator.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 18:39:08 by mairivie          #+#    #+#             */
-/*   Updated: 2025/07/10 18:30:50 by mairivie         ###   ########.fr       */
+/*   Updated: 2025/07/15 16:56:38 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	put_pixel(t_img *image, int color, int x, int y)
 	char	*pix_target;
 
 	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT)
-		return;
+		return ;
 	shifting = y * image->grid_len + x * (image->bit_per_pix / 8);
 	pix_target = image->pix_grid + shifting;
 	*(unsigned int *)pix_target = color;
@@ -47,7 +47,6 @@ void	paint_each_pixel(t_img *ptr, t_infra *skelet)
 	double			y;
 	unsigned int	color;
 
-	
 	x = 0.0;
 	y = HEIGHT;
 	color = 0;
